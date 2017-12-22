260a904d141ae9ba5335206464e3bb4b0ca23583
xinqch <xinchao32119@163.com>
2017-12-23 00:28:16 +0800

    添加tymleaf 页面

--- src/main/java/com/xinqch/springbootarchetype/controller/TestController.java
+++ src/main/java/com/xinqch/springbootarchetype/controller/TestController.java
@@ -3,7 +3,9 @@ package com.xinqch.springbootarchetype.controller;
 import com.xinqch.springbootarchetype.entity.TestTab;
 import com.xinqch.springbootarchetype.service.TestTabService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,22 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
  * @auther xinch
  * @create 2017/12/22 17:15
  */
-@RestController
+@Controller
 public class TestController {
 
     @Autowired
     private TestTabService testTabService;
 
-    @RequestMapping(value = "/getShop")
-    public TestTab getShop(@RequestParam("id") String id) {
-        System.out.println("id = [" + id + "]");
-        TestTab t1 = testTabService.getById(id);
-        System.out.println("id = [" + t1.getId()+"---"+ t1.getMessage()+ "]");
-        TestTab t2 = testTabService.getByIdSlave(id);
-        System.out.println("id = [" + t2.getId()+"---"+ t2.getMessage() + "]");
+//    @RequestMapping(value = "/getShop")
+//    public TestTab getShop(@RequestParam("id") String id) {
+//        System.out.println("id = [" + id + "]");
+//        TestTab t1 = testTabService.getById(id);
+//        System.out.println("id = [" + t1.getId()+"---"+ t1.getMessage()+ "]");
+//        TestTab t2 = testTabService.getByIdSlave(id);
+//        System.out.println("id = [" + t2.getId()+"---"+ t2.getMessage() + "]");
+//
+//        return t1;
+//    }t1
 
-        return t1;
+    @RequestMapping(value = "/getShop", method = RequestMethod.GET)
+    public String getShop() {
+
+        return "test";
     }
-
-
 }
